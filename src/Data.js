4602506d3c7e077784ba71c6e20eee3140474d3c
Evgeniy Kucheruk <kucheruk@gmail.com>
2018-08-02 21:28:17 +0300

    add ponies

---
+++ src/Data.js
@@ -0,0 +1,9 @@
+const data = {
+    skus: [
+        { id: 1, price: 10, weight: 0.1, name: 'Голубой пони', description: 'Мягкий плюшевый голубой пони', img: 'https://i.imgur.com/oCJ72Q1.jpg' },
+        { id: 2, price: 12, weight: 0.1, name: 'Жёлтый пони', description: 'Отстойный но мягкий жёлтый пони', img: 'https://i.imgur.com/ikZ2Efm.jpg' },
+        { id: 3, price: 125, weight: 10, name: 'Розовый пони XXL', description: 'Розовый пони. Большой.', img: 'https://i.imgur.com/xk1SlXy.jpg' }
+    ]
+}
+
+export default data;
